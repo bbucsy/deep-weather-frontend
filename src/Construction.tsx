22e5792764b55cd930dea3ef6bed09f2e5919c21
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-04-26 15:25:00 +0200

    Add construction page

---
+++ src/Construction.tsx
@@ -0,0 +1,10 @@
+import { Center, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export const Construction = () => (
+    <React.StrictMode>
+        <Center>
+            <Heading size="2xl"> Site under construction!</Heading>
+        </Center>
+    </React.StrictMode>
+)
