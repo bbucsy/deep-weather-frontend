44fc47f7be058cf05cfb8b19c860c0112fb36e44
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-05-01 21:44:31 +0200

    Add api key

--- src/service/services.ts
+++ src/service/services.ts
@@ -13,6 +13,9 @@ const configuration = new Configuration({
 export const axiosInstance = axios.create()
 axiosInstance.interceptors.request.use(config => {
     const token = Cookies.get(JWT_TOKEN_COOKIE_KEY)
+    if (process.env.REACT_APP_API_KEY && config.headers) {
+        config.headers['x-api-key'] = process.env.REACT_APP_API_KEY
+    }
     if (token && config.headers) {
         config.headers['Authorization'] = `Bearer ${token}`
     }
@@ -22,7 +25,7 @@ axiosInstance.interceptors.request.use(config => {
 export const CityService = new CityApi(configuration, baseUrl, axiosInstance)
 export const NeuralModelService = new NeuralModelApi(configuration, baseUrl, axiosInstance)
 export const PredictionService = new PredictionsApi(configuration, baseUrl, axiosInstance)
-export const AuthService = new AuthApi(configuration)
+export const AuthService = new AuthApi(configuration, baseUrl, axiosInstance)
 
 export const lagger = async <T>(todo: Promise<T>): Promise<T> => {
     return new Promise<void>(resolve => {
